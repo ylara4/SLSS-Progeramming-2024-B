49f45d4ac44d3f26f86d0fdaf4d6f0b381ada4bc
Yeshua Lara <yeshualara1975@gmail.com>
2024-04-03 12:02:05 -0700

    Filled out notes

--- notes-2-morefunctions.py
+++ notes-2-morefunctions.py
@@ -1,1 +1,41 @@
 # More Functions
+# Yeshua Lara
+# April 3 
+
+# implement stars function
+
+def stars(num):
+    """returns specified numbers of stars"""
+    value = ""  # placeholder for return value
+    # if num is 0, return 1 star
+    # if num is 1, return 1 star
+
+    # elif number is greater than 1, return that num * star
+    
+
+    # else return error saying negative nums arent allowed
+    if num == 0 or num == 1:
+        value = "*"
+    elif num > 1:
+        value = "*" * num
+    else:
+        value = "sorry, cant take negative numbers"
+
+    return value
+
+
+# Multiply strings 
+greeting = "hello"
+print(greeting * 100)
+
+print("the quick brown fox jumps over the lazy dog" * 2)
+
+print(stars(10)) #"**********"
+print(stars(1000)) #"**********"
+print(stars(0))
+
+
+print(stars(0))
+print(stars(1))
+print(stars(1000))
+print(stars(-1))
