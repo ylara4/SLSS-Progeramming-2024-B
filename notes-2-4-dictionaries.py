5e1ed556fe1c571296b12aa9064573170f3a958a
Yeshua Lara <yeshualara1975@gmail.com>
2024-04-15 10:20:39 -0700

    c

---
+++ notes-2-4-dictionaries.py
@@ -0,0 +1,46 @@
+# Dictionaries
+# Yeshua Lara
+# 12 April 
+
+# Big Ideas:
+# - dictionaries are a data structure
+# - dictionaries map keys to values 
+
+# flashback to lists
+person = [
+    "John",
+    "23 years old",
+    "4500 1023 2222 1111",
+    "1.85cm",
+    "Red",
+    "Baking"
+
+]   
+
+
+
+
+# get the persons name and print
+print(person[0])
+print(person[1]) # age?
+print(person[2]) # some numbers?
+print(person[3])
+print(person[4])
+print(person[5])
+
+# flashback to lists
+person = ["John", "23 years old", "4500 1023 2222 1111"]
+
+person_dict = {
+    "name": "John",
+    "age": "23 years old",
+    "credit card num": "4500 1023 2222 1111"
+}
+
+
+
+# create some more key value pairs to our person dictionary
+# once done that, create a "for" loop to iterate over the
+# dictionary and print out the items
+ 
+
