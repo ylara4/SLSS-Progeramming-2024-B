ad65d836b6aa64b70f6b6439c3f8f2ea6e469b2e
Yeshua Lara <yeshualara1975@gmail.com>
2024-04-30 10:22:23 -0700

    g

--- pygame-exercise-snow.py
+++ pygame-exercise-snow.py
@@ -1,5 +1,4 @@
 import random
-
 import pygame as pg
 
 # --CONSTANTS--
@@ -17,6 +16,29 @@ HEIGHT = 720
 SCREEN_SIZE = (WIDTH, HEIGHT)
 
 
+class Snowflake(pg.sprite.Sprite):
+    def __init__(self, size: int):
+        """
+        Params:
+            size: diameter of snowflake in pixels
+        """
+        # Super class constructor
+        super().__init__()
+
+        # Create a blank surface
+        self.image = pg.Surface((size, size))
+
+        # Draw a circle inside of it
+        pg.draw.circle(self.image, WHITE, (size // 2, size // 2), size // 2)
+
+        self.rect = self.image.get_rect()
+
+        # Spawn it in the middle of the screen
+        # Chooses a random x-coordinate
+        self.rect.centerx = random.randrange(0, WIDTH + 1)
+        self.rect.centery = HEIGHT // 2
+
+
 def start():
     """Environment Setup and Game Loop"""
 
@@ -29,9 +51,12 @@ def start():
 
     # All sprites go in this sprite Group
     all_sprites = pg.sprite.Group()
-    
 
-    pg.display.set_caption("<WINDOW TITLE HERE>")
+    # Add 100 snowflakes to the all_sprites Group
+    for _ in range(100):
+        all_sprites.add(Snowflake(10))
+
+    pg.display.set_caption("Snowfall Landscape")
 
     # --Main Loop--
     while not done:
@@ -41,9 +66,14 @@ def start():
                 done = True
 
         # --- Update the world state
+        # Update the state of all sprites
+        all_sprites.update()
 
         # --- Draw items
         screen.fill(BLACK)
+
+        # Draw all the sprites
+        all_sprites.draw(screen)
 
         # Update the screen with anything new
         pg.display.flip()
@@ -58,12 +88,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
 
 
 
